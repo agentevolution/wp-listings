8eb24b8a0bb84d6176b2f2de9e57c32d65a8f6e1
Dave Bonds <dave@agentevolution.com>
2014-04-29 11:15:20 -0500

    update ad metabox and image

--- includes/views/agentevo-metabox.php
+++ includes/views/agentevo-metabox.php
@@ -1,4 +1,2 @@
 <?php
-echo '<br />';
-echo '<a href="http://agentevolution.com" target="_blank"><img src="' . WP_LISTINGS_URL . 'images/ae-website-bug.png" alt ="agent evolution"></a>';
-echo '<p>WordPress Themes for Real Estate Agents</p>';
+echo '<a href="http://www.agentevolution.com" target="_blank"><img src="' . WP_LISTINGS_URL . 'images/ae-themes-ad.png" alt="Premium WordPress Child Themes by Agent Evolution"></a>';
