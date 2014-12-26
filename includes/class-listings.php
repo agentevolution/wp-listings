ac8bea15bf0ad7ec1a97f7b189588c90a6176e9c
Brandon Hubbard <bkhubbard@gmail.com>
2014-12-25 20:52:27 -0800

    Add Jetpack Publicize and Markdown support

--- includes/class-listings.php
+++ includes/class-listings.php
@@ -124,7 +124,7 @@ class WP_Listings {
 				'menu_position'	=> 5,
 				'menu_icon'		=> 'dashicons-admin-home',
 				'has_archive'	=> true,
-				'supports'		=> array( 'title', 'editor', 'author', 'comments', 'excerpt', 'thumbnail', 'revisions', 'equity-layouts', 'equity-cpt-archives-settings', 'genesis-seo', 'genesis-layouts', 'genesis-simple-sidebars', 'genesis-cpt-archives-settings'),
+				'supports'		=> array( 'title', 'editor', 'author', 'comments', 'excerpt', 'thumbnail', 'revisions', 'equity-layouts', 'equity-cpt-archives-settings', 'genesis-seo', 'genesis-layouts', 'genesis-simple-sidebars', 'genesis-cpt-archives-settings', 'publicize', 'wpcom-markdown'),
 				'rewrite'		=> array( 'slug' => $this->options['wp_listings_slug'], 'feeds' => true, 'with_front' => false ),
 			)
 		);
