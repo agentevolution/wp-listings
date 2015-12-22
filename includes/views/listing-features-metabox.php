be80f7d6c715200c5714abb64baa14784119ad64
Dave Bonds <dave@idxbroker.com>
2015-12-22 09:07:31 -0600

    Add filter for additional details (features) metabox

--- includes/views/listing-features-metabox.php
+++ includes/views/listing-features-metabox.php
@@ -1,54 +1,53 @@
 <?php
+function wp_listings_additional_details() {
 
-// Featured on
-global $post;
+	global $post;
 
-echo '<div style="width: 90%; float: left;">';
+	// Create meta box labels and input fields
+	$additonal_details .= '<p>All fields accept text or shortcodes</p>';
+	// Featured On
+	$additonal_details .= '<div style="width: 90%; float: left;">';
+	$additonal_details .= 	sprintf( __( '<p><label>Featured On:<br /><textarea name="wp_listings[_listing_featured_on]" rows="5" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_featured_on', true) ) );
+	$additonal_details .= '</div><br style="clear: both;" />';
 
-	printf( __( '<p><label>Featured on (allows shortcodes):<br /><textarea name="wp_listings[_listing_featured_on]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_featured_on', true) ) );
+	// Home Summary
+	$additonal_details .= '<div style="width: 90%; float: left;">';
+	$additonal_details .= 	sprintf( __( '<p><label>Home Summary:<br /><textarea name="wp_listings[_listing_home_sum]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_home_sum', true) ) );
+	$additonal_details .= '</div><br style="clear: both;" />';
 
-echo '</div><br style="clear: both;" />';
+	// Kitchen Summary
+	$additonal_details .= '<div style="width: 90%; float: left;">';
+	$additonal_details .= 	sprintf( __( '<p><label>Kitchen Summary:<br /><textarea name="wp_listings[_listing_kitchen_sum]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_kitchen_sum', true) ) );
+	$additonal_details .= '</div><br style="clear: both;" />';
 
-// Home Summary
-global $post;
+	// Living Room
+	$additonal_details .= '<div style="width: 90%; float: left;">';
+	$additonal_details .= 	sprintf( __( '<p><label>Living Room:<br /><textarea name="wp_listings[_listing_living_room]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_living_room', true) ) );
+	$additonal_details .= '</div><br style="clear: both;" />';
 
-echo '<div style="width: 90%; float: left;">';
+	// Master Suite
+	$additonal_details .= '<div style="width: 90%; float: left;">';
+	$additonal_details .= 	sprintf( __( '<p><label>Master Suite:<br /><textarea name="wp_listings[_listing_master_suite]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_master_suite', true) ) );
+	$additonal_details .= '</div><br style="clear: both;" />';
 
-	printf( __( '<p><label>Home Summary (allows shortcodes):<br /><textarea name="wp_listings[_listing_home_sum]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_home_sum', true) ) );
+	// School and Neighborhood Info
+	$additonal_details .= '<div style="width: 90%; float: left;">';
+	$additonal_details .= 	sprintf( __( '<p><label>School and Neighborhood Info:<br /><textarea name="wp_listings[_listing_school_neighborhood]" rows="5" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_school_neighborhood', true) ) );
+	$additonal_details .= '</div><br style="clear: both;" />';
 
-echo '</div><br style="clear: both;" />';
+	// Custom Disclaimer
+	$additonal_details .= '<div style="width: 90%; float: left;">';
+	$additonal_details .= 	sprintf( __( '<p><label>Custom Disclaimer:<br /><textarea name="wp_listings[_listing_custom_disclaimer]" rows="5" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_custom_disclaimer', true) ) );
+	$additonal_details .= '</div><br style="clear: both;" />';
 
-// Kitchen Summary
-echo '<div style="width: 90%; float: left;">';
+	// Filter output
+	if( has_filter( 'wp_listings_additional_details_meta_boxes' ) ) {
+		$additonal_details = apply_filters( 'wp_listings_additional_details_meta_boxes', $additonal_details );
+	}
 
-	printf( __( '<p><label>Kitchen Summary (allows shortcodes):<br /><textarea name="wp_listings[_listing_kitchen_sum]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_kitchen_sum', true) ) );
+	return $additonal_details;
 
-echo '</div><br style="clear: both;" />';
+}
 
-// Living Room
-echo '<div style="width: 90%; float: left;">';
-
-	printf( __( '<p><label>Living Room (allows shortcodes):<br /><textarea name="wp_listings[_listing_living_room]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_living_room', true) ) );
-
-echo '</div><br style="clear: both;" />';
-
-// Master Suite
-echo '<div style="width: 90%; float: left;">';
-
-	printf( __( '<p><label>Master Suite (allows shortcodes):<br /><textarea name="wp_listings[_listing_master_suite]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_master_suite', true) ) );
-
-echo '</div><br style="clear: both;" />';
-
-// School and Neighborhood Info
-echo '<div style="width: 90%; float: left;">';
-
-	printf( __( '<p><label>School and Neighborhood Info (allows shortcodes):<br /><textarea name="wp_listings[_listing_school_neighborhood]" rows="5" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_school_neighborhood', true) ) );
-
-echo '</div><br style="clear: both;" />';
-
-// Custom Disclaimer
-echo '<div style="width: 90%; float: left;">';
-
-	printf( __( '<p><label>Custom Disclaimer:<br /><textarea name="wp_listings[_listing_custom_disclaimer]" rows="3" cols="18" style="%s">%s</textarea></label></p>', 'wp_listings' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_custom_disclaimer', true) ) );
-
-echo '</div><br style="clear: both;" />';
+// Output Additional Details meta boxes
+echo wp_listings_additional_details();
