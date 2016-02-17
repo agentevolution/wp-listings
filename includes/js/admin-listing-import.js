29f59f4a8b98f2ac2b274c29b75b6fe0514916e4
Dave Bonds <dave@idxbroker.com>
2016-02-17 15:45:37 -0600

    paired with scrollstop event to improve loading on pages with a gazillion images

--- includes/js/admin-listing-import.js
+++ includes/js/admin-listing-import.js
@@ -2,7 +2,9 @@ jQuery(document).ready(function($) {
 	/* === Begin listing importer JS. === */
 
 	jQuery(function() {
-		jQuery("img.lazy").lazyload();
+		jQuery("img.lazy").lazyload({
+			event: "scrollstop"
+		});
 	});
 
 	jQuery('.grid').masonry({
@@ -65,4 +67,75 @@ jQuery(document).ready(function($) {
 	});
 
 	/* === End listing importer JS. === */
+
+	/* === Scrollstop event ===*/
+	(function(){
+
+	    var special = jQuery.event.special,
+	        uid1 = 'D' + (+new Date()),
+	        uid2 = 'D' + (+new Date() + 1);
+
+	    special.scrollstart = {
+	        setup: function() {
+
+	            var timer,
+	                handler =  function(evt) {
+
+	                    var _self = this,
+	                        _args = arguments;
+
+	                    if (timer) {
+	                        clearTimeout(timer);
+	                    } else {
+	                        evt.type = 'scrollstart';
+	                        jQuery.event.handle.apply(_self, _args);
+	                    }
+
+	                    timer = setTimeout( function(){
+	                        timer = null;
+	                    }, special.scrollstop.latency);
+
+	                };
+
+	            jQuery(this).bind('scroll', handler).data(uid1, handler);
+
+	        },
+	        teardown: function(){
+	            jQuery(this).unbind( 'scroll', jQuery(this).data(uid1) );
+	        }
+	    };
+
+	    special.scrollstop = {
+	        latency: 300,
+	        setup: function() {
+
+	            var timer,
+	                    handler = function(evt) {
+
+	                    var _self = this,
+	                        _args = arguments;
+
+	                    if (timer) {
+	                        clearTimeout(timer);
+	                    }
+
+	                    timer = setTimeout( function(){
+
+	                        timer = null;
+	                        evt.type = 'scrollstop';
+	                        jQuery.event.handle.apply(_self, _args);
+
+	                    }, special.scrollstop.latency);
+
+	                };
+
+	            jQuery(this).bind('scroll', handler).data(uid2, handler);
+
+	        },
+	        teardown: function() {
+	            jQuery(this).unbind( 'scroll', jQuery(this).data(uid2) );
+	        }
+	    };
+
+	})();
 });
